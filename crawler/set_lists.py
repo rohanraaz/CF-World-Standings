67c80a9a39af76798747f45ee73b53fcba4d26d1
Rohan raja <78012250+rohanraaz@users.noreply.github.com>
2022-06-11 12:42:57 +0530

    Update set_lists.py

--- crawler/set_lists.py
+++ crawler/set_lists.py
@@ -1,6 +1,6 @@
 from login import get_csrf
 from get_lists import get_page, get_lists_table, get_lists_urls, \
-                      get_active_countries_dict, get_handles
+                      get_active_organizations_dict, get_handles
 
 # Global Variables #
 lists_url = "https://codeforces.com/lists"
@@ -8,38 +8,38 @@ new_list_url = "https://codeforces.com/lists/new"
 edit_list_url = "https://codeforces.com/data/lists"
 
 
-def create_lists(session, countries_dict):
+def create_lists(session, organizations_dict):
     table = get_lists_table(session, lists_url)
     lists_urls = get_lists_urls(table)
-    active_country_users = get_active_countries_dict(countries_dict)
-    countries_list = []
-    for country in countries_dict:
-        if country not in lists_urls and active_country_users[country] >= 50:
+    active_organization_users = get_active_organizations_dict(organizations_dict)
+    organizations_list = []
+    for organization in organizations_dict:
+        if organization not in lists_urls and active_organization_users[organization] >= 50:
             request = session.get(new_list_url)
             csrf = get_csrf(request)
             params = {'csrf_token': csrf,
                       'action': 'saveList',
-                      'englishName': country,
+                      'englishName': organization,
                       '_tta': 90}
             n = session.post(new_list_url, data=params)
-            countries_list.append(country)
-            print(f"\tList of {country} created! {n}")
+            organizations_list.append(organization)
+            print(f"\tList of {organization} created! {n}")
         else:
-            print(f"\tList of {country} skipped!")
+            print(f"\tList of {organization} skipped!")
 
 
-def clear_diff_lists(session, countries_dict):
+def clear_diff_lists(session, organizations_dict):
     table = get_lists_table(session, lists_url)
     lists_urls = get_lists_urls(table)
-    for country in lists_urls:
-        url = lists_urls[country]
+    for organization in lists_urls:
+        url = lists_urls[organization]
         request = session.get(url)
         csrf = get_csrf(request)
         table = get_lists_table(session, url)
         page = get_page(session, url)
         list_a = page.find('a', {'class': 'delete-user-list-link'})
         list_id = list_a["data-userlistid"]
-        handles = set(get_handles(countries_dict[country]))
+        handles = set(get_handles(organizations_dict[organization]))
         count = 0
         for row in table:
             try:
@@ -55,7 +55,7 @@ def clear_diff_lists(session, countries_dict):
                 count += 1
             except (KeyError, IndexError):
                 pass
-        print(f'\t{count} users of {country} removed!')
+        print(f'\t{count} users of {organization} removed!')
 
 
 def populate_list(session, handles, list_url):
@@ -67,12 +67,12 @@ def populate_list(session, handles, list_url):
     session.post(list_url, data=params)
 
 
-def populate_lists(session, countries_dict):
+def populate_lists(session, organizations_dict):
     table = get_lists_table(session, lists_url)
     lists_urls = get_lists_urls(table)
-    for country in lists_urls:
-        if country in countries_dict:
-            handles = get_handles(countries_dict[country])
+    for organization in lists_urls:
+        if organization in organizations_dict:
+            handles = get_handles(organizations_dict[organization])
             handles = " ".join(handles)
-            populate_list(session, handles, lists_urls[country])
-            print(f"\tList of {country} populated!")
+            populate_list(session, handles, lists_urls[organization])
+            print(f"\tList of {organization} populated!")
