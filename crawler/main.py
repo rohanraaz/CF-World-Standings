76b909bd438e818139c089a3b02bcbb8abbd3a51
Rohan raja <78012250+rohanraaz@users.noreply.github.com>
2022-06-11 12:43:47 +0530

    Update main.py

--- crawler/main.py
+++ crawler/main.py
@@ -1,5 +1,5 @@
 from login import login
-from get_users import get_all_active_users, separate_by_country
+from get_users import get_all_active_users, separate_by_organization
 from set_lists import create_lists, clear_diff_lists, populate_lists
 
 if __name__ == "__main__":
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     print("Getting Users..")
     users = get_all_active_users()
     print("Creating Dictionary..")
-    countries_dict = separate_by_country(users)
+    organizations_dict = separate_by_organization(users)
     print("Creating Lists..")
-    create_lists(session, countries_dict)
+    create_lists(session, organizations_dict)
     print("Emptying Lists..")
-    clear_diff_lists(session, countries_dict)
+    clear_diff_lists(session, organizations_dict)
     print("Populating Lists..")
-    populate_lists(session, countries_dict)
+    populate_lists(session, organizations_dict)
